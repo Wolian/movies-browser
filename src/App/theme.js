6870d05b036a5fb3d4f8cb57bc597e113bf24065
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-25 11:00:13 +0200

    theme.js created

---
+++ src/App/theme.js
@@ -0,0 +1,21 @@
+const colorNames = {
+    black: "#000000",
+    white: "#FFFFFF",
+    mercury: "#E5E5E5",
+    stormGray: "#74788B",
+    mystic: "#E4E6F0",
+    woodSmoke: "#18181B",
+    waterloo: "#7E839A",
+    pattensBlue: "#D6E4FF",
+    scienceBlue: "#0044CC",
+};
+
+const common = {
+    breakpoints: {
+        mobileMax: "767px",
+        tabletVerticalMax: "991px",
+        tabletHorizontalMax: "1199px",
+    },
+    boxShadow: "0px 4px 12px rgba(186, 199, 213, 0.5)",
+    borderRadiusSmall: "5px",
+};
