6ba7b9c003ccb367c3a38ecc5edfd92e7e7e289f
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-07-25 23:26:48 +0200

    Added logo and navigation

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,3 +1,48 @@
-const Navigation = () => <nav></nav>;
+import {
+  Header,
+  Content,
+  Nav,
+  Logo,
+  NameWebsite,
+  Menu,
+  List,
+  Item,
+  Link,
+  LinkActive,
+  Search,
+} from "./styled";
+import logo from "./logo.svg";
+import search from "./search.svg";
+
+const Navigation = () => (
+  <Header>
+    <Content>
+      <Nav>
+        <Logo>
+          <img src={logo} alt="Logo - Movies Browser" width="40" height="40" />
+          <NameWebsite>
+            <Link href="#">Movies Browser</Link>
+          </NameWebsite>
+        </Logo>
+        <Menu>
+          <List>
+            <Item>
+              <LinkActive href="#">MOVIES</LinkActive>
+            </Item>
+            <Item>
+              <Link href="#">PEOPLE</Link>
+            </Item>
+          </List>
+        </Menu>
+      </Nav>
+      <Search>
+        <div>
+          <img src={search} width="18" height="18" alt="" />
+          <input placeholder="Search for movie..." />
+        </div>
+      </Search>
+    </Content>
+  </Header>
+);
 
 export default Navigation;
---
+++ src/common/Navigation/styled.js
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 26px 0;
+  background: ${({ theme }) => theme.colors.woodSmoke};
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1368px;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const NameWebsite = styled.h1`
+  margin: 0;
+  margin-left: 12px;
+  font-size: 24px;
+  font-weight: 500;
+  letter-spacing: -1.5px;
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const Menu = styled.div`
+  margin-left: 80px;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  list-style: none;
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+export const Item = styled.li`
+  padding: 8px 24px;
+`;
+
+export const Link = styled.a`
+  color: ${({ theme }) => theme.colors.white};
+  text-decoration: none;
+  transition: opacity 0.5s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  // Style do linku aktywnego, potrzebne kiedy dodamy React Router.
+  &:active {
+    border: 1px solid ${({ theme }) => theme.colors.white};
+    border-radius: 24px;
+  }
+`;
+
+// To będzie do usunięcia, kiedy wprowadzimy React Router. Jest to tymczasowy zabieg aby pokazać jak będzie wyglądał link aktywny.
+export const LinkActive = styled.a`
+  padding: 8px 24px;
+  color: ${({ theme }) => theme.colors.white};
+  text-decoration: none;
+  transition: opacity 0.5s;
+  border: 1px solid ${({ theme }) => theme.colors.white};
+  border-radius: 24px;
+`;
+
+export const Search = styled.div``;
