d3d8766212ceaaf92513019183d26f31709982f4
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-07-25 21:13:18 +0200

    Create components Navigation

--- src/App/index.js
+++ src/App/index.js
@@ -2,12 +2,14 @@ import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
 import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
+import Navigation from "../common/Navigation";
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Normalize />
       <GlobalStyle />
+      <Navigation />
       <div className="App">
         <h1>Movies Browser</h1>
         <h2>test</h2>
