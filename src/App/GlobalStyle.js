4794aceb92492c2bf0b375ca6071c11a26050c42
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-26 22:38:29 +0200

    background-color changed

--- src/App/GlobalStyle.js
+++ src/App/GlobalStyle.js
@@ -10,7 +10,7 @@ export const GlobalStyle = createGlobalStyle`
     
     body {
         font-family: 'Poppins', sans-serif;
-        background-color: ${({ theme }) => theme.colors.mercury};
+        background-color: ${({ theme }) => theme.colors.whisper};
         color: ${({theme}) => theme.colors.black}
     }
 `;
