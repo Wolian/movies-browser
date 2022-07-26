7ed6d6624aa282bed0f76bc1935a23eebad2fb57
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-26 14:47:30 +0200

    MovieCast component created and styled

---
+++ src/features/movies/MoviePage/MovieCast/styled.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  max-width: 1368px;
+  //width: 100%;
+  margin: 64px auto;
+`;
+
+export const Section = styled.div`
+  margin-top: 64px;
+`;
+
+export const Header = styled.h1`
+  color: ${({ theme }) => theme.colors.black};
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 120%;
+  margin: 0;
+`;
+
+export const Container = styled.div`
+  margin-top: 32px;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-gap: 24px;
+`;
+
+export const Tile = styled.div`
+  background-color: ${({ theme }) => theme.colors.white};
+  padding: 16px;
+  gap: 12px;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+`;
+
+export const Image = styled.img`
+  border-radius: ${({ theme }) => theme.borderRadiusSmall};
+`;
+
+export const Name = styled.p`
+  margin: 14px 0px 0px 0px;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.woodSmoke};
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 130%;
+`;
+
+export const Role = styled.p`
+  margin: 8px 0px 0px 0px;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.waterloo};
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 150%;
+`;
