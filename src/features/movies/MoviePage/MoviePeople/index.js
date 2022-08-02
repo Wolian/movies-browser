62f080d8dd4a6ed640034b8b420ae5782f1d3347
Grzegorz Domin <grze.domin98@gmail.com>
2022-08-02 12:47:01 +0200

    personTile created and moved to common

--- src/features/movies/MoviePage/MoviePeople/index.js
+++ src/features/movies/MoviePage/MoviePeople/index.js
@@ -1,50 +1,21 @@
-import { Wrapper, Header, Container, Tile, Image, Name, Role } from "./styled";
+import { PersonTile } from "../../../../common/PersonTile";
+import { Wrapper, Header, Container } from "./styled";
 
 export const MoviePeople = ({ title }) => {
   return (
     <Wrapper>
       <Header>{title}</Header>
       <Container>
-        <Tile>
-          <Image src="https://i.ibb.co/QHf40YG/poster-1.png" alt="" />
-          <Name>Niki Caro</Name>
-          <Role>Director</Role>
-        </Tile>
-        <Tile>
-          <Image src="https://i.ibb.co/QHf40YG/poster-1.png" alt="" />
-          <Name>Liy Yifei</Name>
-          <Role>Mulan</Role>
-        </Tile>
-        <Tile>
-          <Image src="https://i.ibb.co/QHf40YG/poster-1.png" alt="" />
-          <Name>Liy Yifei</Name>
-          <Role>Mulan</Role>
-        </Tile>
-        <Tile>
-          <Image src="https://i.ibb.co/QHf40YG/poster-1.png" alt="" />
-          <Name>Liy Yifei</Name>
-          <Role>Mulan</Role>
-        </Tile>
-        <Tile>
-          <Image src="https://i.ibb.co/QHf40YG/poster-1.png" alt="" />
-          <Name>Liy Yifei</Name>
-          <Role>Mulan</Role>
-        </Tile>
-        <Tile>
-          <Image src="https://i.ibb.co/QHf40YG/poster-1.png" alt="" />
-          <Name>Liy Yifei</Name>
-          <Role>Mulan</Role>
-        </Tile>
-        <Tile>
-          <Image src="https://i.ibb.co/QHf40YG/poster-1.png" alt="" />
-          <Name>Liy Yifei</Name>
-          <Role>Mulan</Role>
-        </Tile>
-        <Tile>
-          <Image src="https://i.ibb.co/QHf40YG/poster-1.png" alt="" />
-          <Name>Liy Yifei</Name>
-          <Role>Mulan</Role>
-        </Tile>
+        <PersonTile name={"Mulan"} role={"Mulan"} />
+        <PersonTile />
+        <PersonTile />
+        <PersonTile />
+        <PersonTile />
+        <PersonTile />
+        <PersonTile />
+        <PersonTile />
+        <PersonTile />
+        <PersonTile />
       </Container>
     </Wrapper>
   );
