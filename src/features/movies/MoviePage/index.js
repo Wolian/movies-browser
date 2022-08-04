5a67390ae34b635e8911a2e9ec737cf3de6b4d27
Grzegorz Domin <grze.domin98@gmail.com>
2022-08-04 11:49:59 +0200

    dispatching data from saga and API

--- src/features/movies/MoviePage/index.js
+++ src/features/movies/MoviePage/index.js
@@ -1,16 +1,41 @@
-import { MovieCast } from "./MovieCast";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { MoviePeople } from "./MoviePeople";
 import { MoviePoster } from "./MoviePoster";
 import { MovieTile } from "./MovieTile";
 import { Container } from "./styled";
 import Pagination from "../../../common/Pagination";
+import { fetchMovies, selectMovies } from "../moviesSlice";
 
 export const MoviePage = () => {
+  const dispatch = useDispatch();
+  const movies = useSelector(selectMovies);
+
+  useEffect(() => {
+    dispatch(fetchMovies());
+  }, [dispatch]);
+
   return (
     <>
-      <MoviePoster />
+      <MoviePoster
+        poster={movies.backdrop_path}
+        title={movies.title}
+        rate={movies.vote_average}
+        count={movies.vote_count}
+      />
       <Container>
-        <MovieTile />
-        <MovieCast />
+        <MovieTile
+          poster={movies.poster_path}
+          title={movies.title}
+          release={movies.release_date}
+          countries={movies.production_countries}
+          genres={movies.genres}
+          rate={movies.vote_average}
+          count={movies.vote_count}
+          description={movies.overview}
+        />
+        <MoviePeople title={"Cast"} />
+        <MoviePeople title={"Crew"} />
         <Pagination />
       </Container>
     </>
