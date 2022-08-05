35a46dcd1c9a014790d36aa9bcb8f2b370f49fca
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-08-05 20:24:48 +0200

    NoResultsPage component created

---
+++ src/common/NoResultsPage/index.js
@@ -0,0 +1,12 @@
+import { Header, Icon, Wrapper } from "./styled";
+
+const NoResultsPage = () => {
+    return (
+        <Wrapper>
+            <Header>Sorry, there are no results for </Header>
+            <Icon />
+        </Wrapper>
+    );
+};
+
+export default NoResultsPage;
