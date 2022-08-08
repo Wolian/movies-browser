401c864b7e5b83b49db942fdcb3bb1df0443b026
Grzegorz Domin <grze.domin98@gmail.com>
2022-08-08 21:04:12 +0200

    popular Movies displayed with details

--- src/common/MovieTile/index.js
+++ src/common/MovieTile/index.js
@@ -1,40 +1,40 @@
 import {
-    Image,
-    Description,
-    Title,
-    Year,
-    Tags,
-    Tag,
-    Icon,
-    Rating,
-    Rate,
-    Votes,
-    Content,
-    TileWrapper,
-  } from "./styled";
-  import poster from "../images/poster.png";
-  import vector from "../images/vector.png";
-  
-  export const MovieTile = () => {
-    return (
-      <TileWrapper>
-        <Image src={poster} alt="" />
-        <Content>
-          <Description>
-            <Title>Mulan</Title>
-            <Year>2020</Year>
-            <Tags>
-              <Tag>Action</Tag>
-              <Tag>Adventure</Tag>
-              <Tag>Drama</Tag>
-            </Tags>
-          </Description>
-          <Rating>
-            <Icon src={vector} alt="" />
-            <Rate>7,8</Rate>
-            <Votes>35 votes</Votes>
-          </Rating>
-        </Content>
-      </TileWrapper>
-    );
-  };
+  Image,
+  Description,
+  Title,
+  Year,
+  Tags,
+  Tag,
+  Icon,
+  Rating,
+  Rate,
+  Votes,
+  Content,
+  TileWrapper,
+} from "./styled";
+import vector from "../images/vector.png";
+import { APIPhotoUrl } from "../../App/API";
+
+export const MovieTile = ({ poster, title, release, rate, votes }) => {
+  return (
+    <TileWrapper>
+      <Image src={`${APIPhotoUrl}w500${poster}`} alt="" />
+      <Content>
+        <Description>
+          <Title>{title}</Title>
+          <Year>{release ? release.slice(0, 4) : ""}</Year>
+          <Tags>
+            <Tag>Action</Tag>
+            <Tag>Adventure</Tag>
+            <Tag>Drama</Tag>
+          </Tags>
+        </Description>
+        <Rating>
+          <Icon src={vector} alt="" />
+          <Rate>{rate}</Rate>
+          <Votes>{votes} votes</Votes>
+        </Rating>
+      </Content>
+    </TileWrapper>
+  );
+};
--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -1,19 +1,35 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { MovieTile } from "../../../common/MovieTile";
+import { fetchPopularMovies, selectPopularMovies } from "../moviesSlice";
 import { Wrapper, Header, Container } from "./styled";
 
 export const MovieList = () => {
+  const dispatch = useDispatch();
+  const popular = useSelector(selectPopularMovies);
+
+  console.log(popular);
+
+  useEffect(() => {
+    dispatch(fetchPopularMovies());
+  }, [dispatch]);
   return (
     <Wrapper>
       <Header>Popular movies</Header>
       <Container>
-        <MovieTile />
-        <MovieTile />
-        <MovieTile />
-        <MovieTile />
-        <MovieTile />
-        <MovieTile />
-        <MovieTile />
-        <MovieTile />
+        {popular &&
+          popular.results &&
+          popular.results.map((movie) => (
+            <MovieTile
+              key={movie.id}
+              id={movie.id}
+              poster={movie.poster_path}
+              title={movie.title}
+              release={movie.release_date}
+              rate={movie.vote_average}
+              votes={movie.vote_count}
+            />
+          ))}
       </Container>
     </Wrapper>
   );
