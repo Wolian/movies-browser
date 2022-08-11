1da3be553870df29e8e4372714aae6a9b8a78ef4
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-08-11 21:43:14 +0200

    popularPeopleSaga added to rootSaga.js

--- src/App/rootSaga.js
+++ src/App/rootSaga.js
@@ -1,8 +1,9 @@
 import { all } from "redux-saga/effects";
 import { moviesSaga } from "../features/movies/moviesSaga";
-
+import { popularPeopleSaga } from "../features/people/peopleSaga";
+ 
 function* rootSaga() {
-  yield all([moviesSaga()]);
+  yield all([moviesSaga(),popularPeopleSaga()]);
 }
 
 export default rootSaga;
