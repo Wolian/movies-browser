a43baa2afe3ba29b628232aa04e5e87e20c14b13
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-08-11 21:45:27 +0200

    Api people added and pagination added

--- src/features/people/PeopleList/index.js
+++ src/features/people/PeopleList/index.js
@@ -1,10 +1,23 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Wrapper } from "./styled";
 import { MoviePeople } from "../../../common/MoviePeople";
+import { fetchPopularPeople, selectPopularPeople } from "../peopleSlice";
+import Pagination from "../../../common/Pagination";
 
 export const PeopleList = () => {
+  
+  const dispatch = useDispatch();
+  const people = useSelector(selectPopularPeople);
+
+  useEffect(() => {
+    dispatch(fetchPopularPeople());
+  }, [dispatch]);
+
   return (
     <Wrapper>
-      <MoviePeople title={"Popular people"} />
+      <MoviePeople title={"Popular people"} items={people} />
+      <Pagination />
     </Wrapper>
   );
 };
