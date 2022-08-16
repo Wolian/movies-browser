7f2a97d830926e1902bc44c01fbb6d70967b6103
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-08-16 21:19:12 +0200

    Replace the text in the input field

--- src/common/Search/index.js
+++ src/common/Search/index.js
@@ -1,11 +1,17 @@
 import { Wrapper, IconSearch, Input } from "./styled";
 import search from "./search.svg";
+import { useLocation } from "react-router-dom";
 
-const Search = () => (
-  <Wrapper>
-    <IconSearch src={search} alt="" />
-    <Input placeholder="Search for movie..." />
-  </Wrapper>
-);
+const Search = () => {
+  const typePage = useLocation().pathname.split("/")[1];
+  console.log(typePage);
+
+  return (
+    <Wrapper>
+      <IconSearch src={search} alt="" />
+      <Input placeholder={`Search for ${typePage}...`} />
+    </Wrapper>
+  );
+};
 
 export default Search;
