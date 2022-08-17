483c3440f9cf4b8f5d5865f9b1391f8cd58f588a
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-08-17 21:53:58 +0200

    Create component SearchResult

--- src/common/Search/index.js
+++ src/common/Search/index.js
@@ -1,15 +1,42 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { searchQueryParamName } from "../../features/SearchResult/searchQueryParamName";
 import { Wrapper, IconSearch, Input } from "./styled";
-import search from "./search.svg";
-import { useLocation } from "react-router-dom";
+import searchIcon from "./search.svg";
 
 const Search = () => {
-  const typePage = useLocation().pathname.split("/")[1];
-  console.log(typePage);
+  const location = useLocation();
+  const locationPathName = location.pathname;
+  let navigate = useNavigate();
+
+  let typePage = locationPathName.split("/")[1];
+  if (typePage === "" || (typePage !== "movies" && typePage !== "people")) {
+    typePage = "movies";
+  }
+
+  const query = new URLSearchParams(location.search).get(searchQueryParamName);
+
+  const onInputChanged = ({ target }) => {
+    const searchParams = new URLSearchParams(location.search);
+
+    if (target.value.trim() === "") {
+      searchParams.delete(searchQueryParamName);
+    } else {
+      searchParams.get(searchQueryParamName, target.value);
+    }
+
+    navigate(`${locationPathName}?${searchParams.toString()}`);
+  };
 
   return (
     <Wrapper>
-      <IconSearch src={search} alt="" />
-      <Input placeholder={`Search for ${typePage}...`} />
+      <IconSearch src={searchIcon} alt="" />
+      <Input
+        value={query == null ? "" : query}
+        onChange={onInputChanged}
+        placeholder={`Search for ${typePage}...`}
+        type="text"
+        max="50"
+      />
     </Wrapper>
   );
 };
