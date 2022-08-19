72585fae3ddbc8c9b6fd68e95296133a707ac293
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-08-19 23:22:32 +0200

    Delete margin top

--- src/common/NoResultsPage/styled.js
+++ src/common/NoResultsPage/styled.js
@@ -2,36 +2,33 @@ import styled from "styled-components";
 import { ReactComponent as NoResultsIcon } from "./Icon.svg";
 
 export const Wrapper = styled.main`
-    margin: 56px auto 0 auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 1384px;
-    
+  margin: 0 auto 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 1384px;
 `;
 
 export const Icon = styled(NoResultsIcon)`
-    width: 600px;
-    
+  width: 600px;
 `;
 
 export const Header = styled.h1`
-    font-weight: 600;
-    font-size: 36px;
-    line-height: 120%;
-    align-self: flex-start;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 120%;
+  align-self: flex-start;
 
-    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
-        font-size: 30px;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    font-size: 30px;
+  }
 
-    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
-        font-size: 24px;
-    }
-    
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 24px;
+  }
 `;
 
 export const Photo = styled.div`
-    text-align: center;
+  text-align: center;
 `;
