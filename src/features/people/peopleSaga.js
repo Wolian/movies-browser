d8cec0f57ecd7d13de40a3e5a6cd3417a37d94b6
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-08-23 16:34:49 +0200

    peopleSaga updated

--- src/features/people/peopleSaga.js
+++ src/features/people/peopleSaga.js
@@ -1,36 +1,36 @@
 import axios from "axios";
 import { put, takeEvery } from "redux-saga/effects";
 import { APIKey, APIUrl } from "../../App/API";
-import { 
-    fetchPopularPeople, 
-    setPopularPeople,  
-    fetchPeopleCrewCast, 
-    setPeopleCrewCast,
+import {
+  fetchPopularPeople,
+  setPopularPeople,
+  fetchPeopleCrewCast,
+  setPeopleCrewCast,
 } from "./peopleSlice";
 
 function* fetchPopularPeopleHandler() {
-    try {
-        const popularPeople = yield axios.get(
-            `${APIUrl}person/popular?api_key=${APIKey}`
-        );
-        yield put(setPopularPeople(popularPeople.data.results));
-    } catch (error) {
-        yield console.log("error", error);
-    }
+  try {
+    const popularPeople = yield axios.get(
+      `${APIUrl}person/1281?api_key=${APIKey}`
+    );
+    yield put(setPopularPeople(popularPeople.data));
+  } catch (error) {
+    yield console.log("error", error);
+  }
 }
 
 function* fetchPeopleHandlerCrewCast({ payload: id }) {
-    try {
-      const people = yield axios.get(
-        `${APIUrl}movie/${id}/credits?api_key=${APIKey}&language=en-US`
-      );
-      yield put(setPeopleCrewCast(people.data));
-    } catch (error) {
-      yield console.log("error", error);
-    }
+  try {
+    const people = yield axios.get(
+      `${APIUrl}movie/${id}person/{person_id}?api_key=${APIKey}&language=en-US`
+    );
+    yield put(setPeopleCrewCast(people.data));
+  } catch (error) {
+    yield console.log("error", error);
   }
+}
 
 export function* popularPeopleSaga() {
-    yield takeEvery(fetchPopularPeople.type, fetchPopularPeopleHandler);
-    yield takeEvery(fetchPeopleCrewCast.type, fetchPeopleHandlerCrewCast);
+  yield takeEvery(fetchPopularPeople.type, fetchPopularPeopleHandler);
+  yield takeEvery(fetchPeopleCrewCast.type, fetchPeopleHandlerCrewCast);
 }
