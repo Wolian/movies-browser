fb35aa85bf5fbd40d1a19694d823cb73143541f0
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-08-23 16:34:39 +0200

    personDetail styled

---
+++ src/common/PersonDetail/styled.js
@@ -0,0 +1,140 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  background-color: ${({ theme }) => theme.colors.white};
+  max-width: 1368px;
+  //width: 100%;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-areas:
+    "poster content content"
+    "poster description description";
+  grid-column-gap: 40px;
+  padding: 40px;
+  margin: 64px auto;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    margin: 21px 0;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    grid-template-areas:
+      "poster content"
+      "poster content"
+      "description description";
+    padding: 16px;
+    gap: 16px;
+    margin: 16px 0;
+  } ;
+`;
+
+export const Poster = styled.img`
+  grid-area: poster;
+  border-radius: ${({ theme }) => theme.borderRadiusSmall};
+  max-width: 312px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    max-width: 92%;
+    max-height: 92%;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    max-width: 114px;
+    max-height: 169px;
+  } ;
+`;
+
+export const InfoWrapper = styled.div`
+  grid-area: content;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 8px 0px;
+  gap: 24px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    padding: 0;
+    gap: 8px;
+  } ;
+`;
+
+export const Name = styled.h1`
+  font-size: 36px;
+  font-weight: 600;
+  line-height: 120%;
+  margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-weight: 500;
+    font-size: 4.5vw;
+    line-height: 130%;
+  } ;
+`;
+
+
+
+export const SectionBirth = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-weight: 400;
+    line-height: 130%;
+  } ;
+`;
+
+export const Section = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: 10px;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 120%;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 12px;
+  } ;
+`;
+
+export const Birth = styled.p`
+  font-size: 18px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.stormGray};
+  margin: 0;
+`;
+
+export const Date = styled.span`
+  color: ${({ theme }) => theme.colors.black};
+  margin: 0;
+`;
+
+export const Span = styled.span`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 120%;
+  margin-left: 8px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    display: none;
+  } ;
+`;
+
+
+
+export const Biography = styled.div`
+  grid-area: description;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 160%;
+  color: ${({ theme }) => theme.colors.black};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 160%;
+    word-wrap: break-word;
+  } ;
+`;
