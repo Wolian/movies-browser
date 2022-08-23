aea5f857150536eaf5615c653ed3e299872d48ae
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-08-23 16:34:03 +0200

    Route added

--- src/App/index.js
+++ src/App/index.js
@@ -2,6 +2,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import { MoviePage } from "../features/movies/MoviePage";
 import { MovieList } from "../features/movies/MovieList";
 import { PeopleList } from "../features/people/PeopleList";
+import { PeopleProfile } from "../features/people/PeopleProfile";
 import Navigation from "../common/Navigation";
  
 export const App = () => {
@@ -12,6 +13,7 @@ export const App = () => {
       <Route path="/" element={<MovieList />} />
       <Route path="/movies" element={<MovieList />} />
       <Route path="/people" element={<PeopleList />} />
+      <Route path="/peopleprofile" element={<PeopleProfile />} />
       <Route path="/moviepage/:id" element={<MoviePage />} />
     </Routes>
   </HashRouter>
