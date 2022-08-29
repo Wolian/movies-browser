c761cc5395984ef0271613905769304a16c99980
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-08-29 23:01:09 +0200

    Link to peopleprofile added

--- src/common/PersonTile/index.js
+++ src/common/PersonTile/index.js
@@ -1,12 +1,16 @@
 import { APIPhotoUrl } from "../../App/API";
 import { Tile, Image, Name, Role } from "./styled";
 import noPersonPhoto from "./noPersonPhoto.svg";
+import { Link } from "react-router-dom";
 
-export const PersonTile = ({ name, role, photo }) => {
+
+export const PersonTile = ({ name, role, photo, personId }) => {
   const image = `${APIPhotoUrl}w185${photo}`;
   return (
     <Tile>
-      <Image src={photo ? image : noPersonPhoto} alt={name} />
+      <Link to={`/peopleprofile/${personId}`}>
+        <Image src={photo ? image : noPersonPhoto} alt={name} />
+      </Link>
       <Name>{name}</Name>
       <Role>{role}</Role>
     </Tile>
