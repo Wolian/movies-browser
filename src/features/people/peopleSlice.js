40908e2a19cb8b71d93396afd1f3eee664cbf0b8
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-08-29 23:01:27 +0200

    peopleSlice updated

--- src/features/people/peopleSlice.js
+++ src/features/people/peopleSlice.js
@@ -4,11 +4,16 @@ const peopleSlice = createSlice({
     name: "popularPeople",
     initialState: {
         popularPeople: [],
+        personDetail: [],
     },
     reducers: {
         fetchPopularPeople: () => { },
         setPopularPeople: (state, { payload: popularPeople }) => {
             state.popularPeople = popularPeople;
+        },
+        fetchPersonDetail: () => { },
+        setPersonDetail: (state, {payload: personDetail}) => {
+            state.personDetail = personDetail
         },
         fetchPeopleCrewCast: () => { },
         setPeopleCrewCast: (state, { payload: peopleCrewCast }) => {
@@ -19,13 +24,17 @@ const peopleSlice = createSlice({
 
 export const selectPopularPeopleState = (state) => state.popularPeople;
 export const selectPeopleCrewCastState = (state) => state.people;
+export const selectPersonDetailState = (state) => state.personDetail;
 
 export const selectPopularPeople = (state) => selectPopularPeopleState(state).popularPeople;
 export const selectPeopleCrewCast = (state) => selectPeopleCrewCastState(state).peopleCrewCast;
+export const selectPersonDetail = (state) => selectPersonDetail(state).personDetail;
 
 export const {
     fetchPopularPeople,
     setPopularPeople,
+    fetchPersonDetail,
+    setPersonDetail,
     fetchPeopleCrewCast,
     setPeopleCrewCast,
 } = peopleSlice.actions;
