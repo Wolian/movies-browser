5c12207aa0aad787f0647795532142eae9942cf1
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-08-31 21:56:57 +0200

    fix

--- src/features/people/peopleSaga.js
+++ src/features/people/peopleSaga.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { put, takeEvery} from "redux-saga/effects";
+import { put, takeEvery } from "redux-saga/effects";
 import { APIKey, APIUrl } from "../../App/API";
 import {
   fetchPopularPeople,
