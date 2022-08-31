2b331e11c5eb54368d93a3d95bf53bbddcad9695
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-08-31 20:51:15 +0200

    reducer added to store

--- src/App/store.js
+++ src/App/store.js
@@ -14,6 +14,7 @@ const store = configureStore({
     genres: moviesReducer,
     popularPeople: peopleReducer,
     personDetail: peopleReducer,
+    personCastCrew: peopleReducer,
   },
   middleware: [sagaMiddleware],
 });
