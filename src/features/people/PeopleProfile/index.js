7290027b6d5d8e093b95b926eecda9c98b58e8a4
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-08-31 20:52:36 +0200

    Movies cast are showed in PersonDetail

--- src/features/people/PeopleProfile/index.js
+++ src/features/people/PeopleProfile/index.js
@@ -1,29 +1,49 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { Wrapper } from "./styled";
-import { fetchPersonDetail, selectPersonDetail } from "../peopleSlice";
+import { CastSection, CrewSection, Wrapper } from "./styled";
+import { fetchPersonCastCrew, fetchPersonDetail, selectPersonCastCrew, selectPersonDetail } from "../peopleSlice";
 import { PersonDetail } from "../../../common/PersonDetail";
+import { MovieTile } from "../../../common/MovieTile";
 
 export const PeopleProfile = () => {
-  
+
   const { id } = useParams();
   const dispatch = useDispatch();
   const person = useSelector(selectPersonDetail);
+  const castCrew = useSelector(selectPersonCastCrew);
 
   useEffect(() => {
     dispatch(fetchPersonDetail(id));
+    dispatch(fetchPersonCastCrew(id));
   }, [id, dispatch]);
 
   return (
     <Wrapper>
       <PersonDetail
-          poster={person.profile_path}
-          name={person.name}
-          birthDate={person.birthday}
-          birthPlace={person.place_of_birth}
-          biography={person.biography}
-        />
+        poster={person.profile_path}
+        name={person.name}
+        birthDate={person.birthday}
+        birthPlace={person.place_of_birth}
+        biography={person.biography}
+      />
+      <h2>Movies-cast ({castCrew.cast.length})</h2>
+      <CastSection>
+        {castCrew.cast?.map((cast) => (
+          <MovieTile
+            key={cast.id}
+            id={cast.id}
+            poster={cast.poster_path}
+            title={cast.title}
+            release={cast.release_date}
+            genres={cast.genre_ids}
+            movieGenre={cast.genre_ids}
+            rate={cast.vote_average}
+            votes={cast.vote_count}
+          />
+        ))}
+      </CastSection>
+
     </Wrapper>
   );
 };
--- src/features/people/PeopleProfile/styled.js
+++ src/features/people/PeopleProfile/styled.js
@@ -5,3 +5,17 @@ export const Wrapper = styled.main`
   margin: 0 auto;
   padding: 0 10px;
 `;
+
+export const CastSection = styled.section`
+  max-width: 1368px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(292px, 1fr));
+  grid-gap: 24px;
+`;
+
+export const CrewSection = styled.section`
+  max-width: 1368px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(292px, 1fr));
+  grid-gap: 24px;
+`;
