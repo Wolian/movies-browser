ce7c9427c9580f894196cc636515937e5dadc7b4
Krzysztof Banasiak <krzys_banasiak@o2.pl>
2022-08-31 21:57:45 +0200

    TileCastCrew style created

--- src/features/people/PeopleProfile/styled.js
+++ src/features/people/PeopleProfile/styled.js
@@ -6,16 +6,13 @@ export const Wrapper = styled.main`
   padding: 0 10px;
 `;
 
-export const CastSection = styled.section`
-  max-width: 1368px;
+export const CastCrewSection = styled.section`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(292px, 1fr));
   grid-gap: 24px;
 `;
 
-export const CrewSection = styled.section`
-  max-width: 1368px;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(292px, 1fr));
-  grid-gap: 24px;
+export const TileCastCrew = styled.h2`
+  font-size: 36px;
+  margin-top: 64px;
 `;
