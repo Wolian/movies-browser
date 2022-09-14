ce205441d8983274e7df70dc3e330c19d5ea2ca2
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-09-14 21:46:37 +0200

    Added link for name

--- src/common/PersonTile/index.js
+++ src/common/PersonTile/index.js
@@ -1,16 +1,17 @@
-import { Link } from "react-router-dom";
 import { APIPhotoUrl } from "../../App/API";
-import { Tile, Image, Name, Role } from "./styled";
+import { Tile, LinkPerson, Image, Name, Role } from "./styled";
 import noPersonPhoto from "./noPersonPhoto.png";
 
 export const PersonTile = ({ name, role, photo, personId }) => {
   const image = `${APIPhotoUrl}w500${photo}`;
   return (
     <Tile>
-      <Link to={`/peopleprofile/${personId}`}>
+      <LinkPerson to={`/peopleprofile/${personId}`}>
         <Image src={photo ? image : noPersonPhoto} alt={name} />
-      </Link>
-      <Name>{name}</Name>
+      </LinkPerson>
+      <LinkPerson to={`/peopleprofile/${personId}`}>
+        <Name>{name}</Name>
+      </LinkPerson>
       <Role>{role}</Role>
     </Tile>
   );
